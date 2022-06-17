0ebae02904927d55eccf7f7ec5a75a3307f95fd4
Raviganesh M <raviganeshmbhat999@gmail.com>
2022-06-17 11:36:34 +0530

    useInput custom hook

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
+import useInput from "../hooks/useInput";
 
 function UserForm() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [firstName, bindFirstName, resetFirstName] = useInput("");
+  const [lastName, bindLastName, resetLastName] = useInput("");
 
   const submitHandler = (e) => {
     e.preventDefault();
     alert(`Hello ${firstName} ${lastName}`);
+    resetFirstName();
+    resetLastName();
   };
 
   return (
@@ -14,19 +17,11 @@ function UserForm() {
       <form onSubmit={submitHandler}>
         <div>
           <label>First Name</label>
-          <input
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            type="text"
-          />
+          <input {...bindFirstName} type="text" />
         </div>
         <div>
           <label>Last Name</label>
-          <input
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            type="text"
-          />
+          <input {...bindLastName} type="text" />
         </div>
         <button type="submit">Submit</button>
       </form>
